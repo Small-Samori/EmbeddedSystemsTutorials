9811a36b6b27570a80c29d7fae6a305e6916e253
Ronald Arthur <esarearthur@gmail.com>
2019-07-11 15:11:04 +0000

    Added GPIO and ADC Projects

---
+++ ADC/ArduinoSketch1/Blank/Sketch.cpp
@@ -0,0 +1,16 @@
+﻿/*Begining of Auto generated code by Atmel studio */
+#include <Arduino.h>
+
+/*End of auto generated code by Atmel studio */
+
+uint16_t temperature = 0;
+
+void setup() {
+	// put your setup code here, to run once:
+	temperature = analogRead(A3) * 0.488;
+}
+
+void loop() {
+	// put your main code here, to run repeatedly:
+
+}
