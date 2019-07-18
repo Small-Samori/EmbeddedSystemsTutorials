61998e6d1aaf62065af5d10c5b53a195f1feef00
Ronald Arthur <esarearthur@gmail.com>
2019-07-18 11:44:46 +0000

    Added group project

---
+++ Incubator - G2/ArduinoSketch1/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 7/11/2019 2:23:49 PM
+ * Author : Ronald Arthur
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ Incubator - G3/ArduinoSketch1/Blank/Debug/makedep.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+Sketch.cpp
+
+src\libraries\HX711\HX711.cpp
+
+src\libraries\LiquidCrystal_I2C\LiquidCrystal_I2C.cpp
+
+src\libraries\PulseSensor_Playground\src\PulseSensorPlayground.cpp
+
+src\libraries\PulseSensor_Playground\src\utility\PulseSensor.cpp
+
+src\libraries\PulseSensor_Playground\src\utility\PulseSensorSerialOutput.cpp
+
+src\libraries\PulseSensor_Playground\src\utility\PulseSensorTimingStatistics.cpp
+
+src\libraries\SimpleDHT\SimpleDHT.cpp
+
+src\libraries\Wire\utility\twi.c
+
+src\libraries\Wire\Wire.cpp
+
